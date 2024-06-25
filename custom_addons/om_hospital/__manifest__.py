6c1718d01962aefa9119ef295d31088ecd98ea9c
prasiddhabhaktakhawas <prasiddhakhawas@gmail.com>
2024-06-25 19:35:22 +0545

    menu view, patient view created, security basic implemented

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -1,4 +1,19 @@
 {
     'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': 'Prasiddha',
     'sequence': -100,
+    'summary': 'Hospital management system',
+    'description': """Hospital Management System""",
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_view.xml',
+    ],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
 }
