f6049ee2a799669b43c2d470073cef2bd8c24780
prasiddhabhaktakhawas <prasiddhakhawas@gmail.com>
2024-06-26 19:31:17 +0545

    related fields

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -7,5 +7,6 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin'] #inherited models from mail module
 
     patient_id = fields.Many2one('hospital.patient', string="Patient") #all the records of patients will be shown in drop down menu
+    gender=fields.Selection(related='patient_id.gender') #automatically gets gender from patient_id
     appointment_time = fields.Datetime(string="appointment time", default=fields.Datetime.now)
     booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
