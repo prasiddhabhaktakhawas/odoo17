f8e064dd08b703a3c57076f631849246a7a9c9ef
prasiddhabhaktakhawas <prasiddhakhawas@gmail.com>
2024-06-26 21:55:07 +0545

    computed field

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from datetime import date
 
 
 class HospitalPatient(models.Model):
@@ -7,7 +8,16 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin'] #inherited models from mail module
 
     name = fields.Char(string='Name', tracking=True)
+    date_of_birth = fields.Date(string='Date of brith')
     ref = fields.Char(string='Ref', defualt="Odoo Mates")
-    age = fields.Integer(string="Age", tracking=True)
+    age = fields.Integer(string="Age", compute='_compute_age', tracking=True)
     gender = fields.Selection([('male','Male'),('female','Female')], string="Gender", tracking=True, default="female")
     active = fields.Boolean(string="Active", default=True) #used for archiving
+    @api.depends('date_of_birth') #resonsible for instant change in computed field. otherwise, the field value only changes when saved
+    def _compute_age(self): # computes age automatically
+        for rec in self:    # self represents all the records, so we need to loop
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age= today.year - rec.date_of_birth.year
+            else:
+                rec.age = 0
