425e7c09283490c1e5d00155e30496aae3101aaa
prasiddhabhaktakhawas <prasiddhakhawas@gmail.com>
2024-06-26 17:31:00 +0545

    chatter added

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -4,6 +4,7 @@ from odoo import api, fields, models
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _description = "Hospital Patient"
+    _inherit = ['mail.thread', 'mail.activity.mixin'] #inherited models from mail module
 
     name = fields.Char(string='Name')
     ref = fields.Char(string='Ref')
