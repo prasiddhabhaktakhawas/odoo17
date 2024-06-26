1adc41c7b4176d271a03702fbfed021f0c9cbb80
prasiddhabhaktakhawas <prasiddhakhawas@gmail.com>
2024-06-26 17:44:32 +0545

    search panel added

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -6,8 +6,8 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient"
     _inherit = ['mail.thread', 'mail.activity.mixin'] #inherited models from mail module
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name', tracking=True)
     ref = fields.Char(string='Ref')
-    age = fields.Integer(string="Age")
-    gender = fields.Selection([('male','Male'),('female','Female')], string="Gender")
+    age = fields.Integer(string="Age", tracking=True)
+    gender = fields.Selection([('male','Male'),('female','Female')], string="Gender", tracking=True)
     active = fields.Boolean(string="Active", default=True)
