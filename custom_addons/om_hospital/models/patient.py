ab936092116ff61f7abbcb4d0efa98ab1194b386
prasiddhabhaktakhawas <prasiddhakhawas@gmail.com>
2024-06-28 16:17:56 +0545

    adding image avatar in a field

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -14,6 +14,7 @@ class HospitalPatient(models.Model):
     gender = fields.Selection([('male','Male'),('female','Female')], string="Gender", tracking=True, default="female")
     active = fields.Boolean(string="Active", default=True) #used for archiving
     appointment = fields.Many2one('hospital.appointment',string='Appointments')
+    image=fields.Image(string="Image")
 
     @api.depends('date_of_birth') #resonsible for instant change in computed field. otherwise, the field value only changes when saved
     def _compute_age(self): # computes age automatically
